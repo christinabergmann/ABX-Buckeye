6b89b01b7f5dd50888c6ea57c7ec6e8d603bfd3c
christinabergmann <chbergma@uos.de>
2016-09-13 17:33:11 +0200

    Item files for complete Buckeye

--- Supplementary/PrepBuckeye copy.R
+++ Supplementary/PrepBuckeye copy.R
@@ -1,25 +1,29 @@
-# Separate the zerospeech item file into vowels and consonants and impose a minimum threshold per speaker / context combination
+# Separate the full corpus item file into vowels and consonants and impose a minimum threshold per speaker / context combination
 
 
-# Download the file from:
+buck = read.table("Supplementary/triphones.item")
 
-buck = read.table("english.item")
 
-vowels = c("aa", "ae", "ah", "aw", "ay", "eh", "er", "ey", "ih", "iy", "ow", "oy", "uh", "uw")
-
-# These are consequently the consonants: "b"  "ch" "d"  "dh" "el" "en" "f"  "g"  "hh" jh" "k"  "l"  "m"  "n"  "ng" "p"  "r"  "s"  "sh" "t"  "th" "z"  "zh" "v"  "w"  "y"
-
-#in the actual item file, the header is:
-#file onset offset #phone context talker
+#in the actual item file, theheader is:
+##file onset offset #phone prev-phone next-phone speaker
 #Unfortunately "#" has different meaning in R, so we omit them for now and only add them back in when writing the file
-names(buck) = c("file", "onset", "offset", "phone", "context", "talker")
-
+names(buck) = c("file", "onset", "offset", "phone", "p_context", "n_context", "talker")
 
 buck$talker = as.factor(buck$talker)
 
+buck$context <- paste(buck$p_context, buck$n_context, sep = "-")
+buck$context = as.factor(buck$context)
+
+buck = subset(buck, select = -c(p_context, n_context))
+
+# We need both since there are additional symbols in the corpus, like silence, or noise
+vowels = c("aa", "ae", "ah", "ao", "aw", "ay", "eh", "er", "ey", "ih", "iy", "ow", "oy", "uh", "uw")
+consonants = c("b", "ch", "d", "dh", "dx", "el", "em", "en", "eng", "f", "g", "hh", "jh", "k", "l", "m", "n", "ng", "nx", "p", "r", 
+               "s", "sh", "t", "th", "tq", "v", "w", "y", "z", "zh")
+
 #Now we create two subsets
 buck.vowels = droplevels(subset(buck, phone %in% vowels))
-buck.cons = droplevels(subset(buck, !(phone %in% vowels)))
+buck.cons = droplevels(subset(buck, phone %in% consonants))
 
 
 #Here we create the two files in the suitable format for ABXpy
