656d3948f618d15f0c5b03db756d66728c1e7e50
alecristia <alecristia@gmail.com>
2017-12-25 13:48:25 +0100

    added regressions within buckeye and across buckeye N brent

---
+++ Scripts/Brent_Buckeye_Comp.R
@@ -0,0 +1,42 @@
+# Author: Alejandrina Cristia (alecristia 'at' gmail)
+# Last modified: 25.12.2017
+# Description: Calculate model to decide whether talker effects are larger or smaller than contrast or corpus effects
+
+
+read.table("Supplementary/both_buckeye.txt",header=T)->buck
+buck$corpus="buckeye"
+
+read.table("../ABX-Brent/Supplementary/both_brent.txt",header=T)->brent
+brent$corpus="brent"
+
+all=rbind(buck,brent)
+levels(brent$contrasts)[!(levels(brent$contrasts) %in% levels(buck$contrasts))]
+#problem: not only speakers in one corpus are not in the other corpus but also slight differences in contrast coverage
+
+inbothcor=rownames(table(all$contrasts,all$corpus))[table(all$contrasts,all$corpus)[,1]>0 & table(all$contrasts,all$corpus)[,2]>0]
+
+all_ol=subset(all,contrasts %in% inbothcor)
+
+whole=lm((mean_score_temp-5)~contrasts+corpus+speakerpairs,data=all_ol)
+wo_sp=lm((mean_score_temp-5)~contrasts+corpus,data=all_ol)
+wo_contrasts=lm((mean_score_temp-5)~speakerpairs+corpus,data=all_ol)
+wo_corpus=lm((mean_score_temp-5)~speakerpairs+contrasts,data=all_ol)
+
+summary(whole) #49% of var explained
+
+anova(whole,wo_sp) #effect of speaker removal F 10.7
+summary(wo_sp) #41% var explained
+
+anova(whole,wo_contrasts) #effect of contrast removal F 121.7
+summary(wo_contrasts) #20% var explained
+
+anova(whole,wo_corpus) #effect of corpus removal NA
+summary(wo_corpus) #49% var explained
+
+whole=lm((mean_score_temp-5)~contrasts*corpus+speakerpairs,data=all_ol)
+wo_sp=lm((mean_score_temp-5)~contrasts*corpus,data=all_ol)
+summary(whole) #60% of var explained
+
+anova(whole,wo_sp) #effect of speaker removal F 13.6
+summary(wo_sp) #51% var explained
+
---
+++ Scripts/DoInferStats.R
@@ -0,0 +1,40 @@
+# Author: Alejandrina Cristia (alecristia 'at' gmail), largely from Christina Bergmann's code
+# Last modified: 25.12.2017
+# Description: Calculate model to decide whether between speaker leads to worse scores than within
+
+source("scripts/Readin.R")
+
+library(dplyr)
+
+#For sampling later we need to add this column
+rawdata_within$speakerpairs = paste(rawdata_within$speaker, rawdata_within$speaker, sep="-")
+
+#Average over contexts to have a mean score  per contrast-speakerpair
+between_percontrastspeaker = rawdata_between %>%
+  group_by(speakerpairs, contrasts) %>%
+  summarise(mean_score_temp = mean(score)) %>%
+  ungroup() 
+
+within_percontrastspeaker = rawdata_within %>%
+  group_by(speakerpairs, contrasts) %>%
+  summarise(mean_score_temp = mean(score)) %>%
+  ungroup() 
+
+both=rbind(between_percontrastspeaker,within_percontrastspeaker)
+both$sp_type=ifelse(substr(both$speakerpairs,1,2)==substr(both$speakerpairs,4,5),"w","b")
+both$c_type<-"C"
+both$c_type[grep("[aeiou]",both$contrasts)]<-"V"
+both=data.frame(both)
+#The goal is to assess the size of the effect for sp_type (between versus within) versus sound contrast
+
+whole=lm(mean_score_temp~contrasts+speakerpairs,data=both)
+contrast_only=lm(mean_score_temp~contrasts,data=both)
+speakers_only=lm(mean_score_temp~speakerpairs,data=both)
+anova(whole,contrast_only)
+anova(whole,speakers_only)
+summary(contrast_only) #contrast ID explains 53% of the variance
+summary(speakers_only) #speaker ID explains 16% of the variance  - 4x less !!
+#and notice that this includes both individual variability and that related to within vs across
+summary(whole) #the model with both explains 48% of the variance 
+
+write.table(both,"Supplementary/both_buckeye.txt",row.names=F,sep="\t")
